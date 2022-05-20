2e61a55901ae74bcb2e6bf5f30da0927fc1d2395
RodrigoUngo <00075419@uca.edu.sv>
2022-05-20 11:43:38 -0600

    Add api

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const helmet = require("helmet");
+require('./database/config');
+const userRouter = require("./routers/user");
+const wordRouter = require("./routers/word");
+const passport = require("passport");
+require("./auth/auth");
+const authRouter = require('./routers/auth');
+
+const app = express();
+app.use(logger("dev"));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(authRouter);
+app.use(passport.authenticate("jwt", { session: false }))
+app.use("/users", userRouter);
+app.use("/words", wordRouter);
+
+module.exports = app;
---
+++ auth/auth.js
@@ -0,0 +1,28 @@
+const passport = require("passport");
+const UserModel = require("../models/user");
+const JWTstrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET || "TOP_SECRET",
+      jwtFromRequest: ExtractJWT.fromExtractors([
+        ExtractJWT.fromUrlQueryParameter("secret_token"),
+        ExtractJWT.fromHeader("secret_token"),
+        ExtractJWT.fromAuthHeaderAsBearerToken(),
+      ]),
+    },
+    async (token, done) => {
+      try {
+        const user = await UserModel.findOne(
+          { _id: token.user._id },
+          "-password"
+        );
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
